4ecf35c848cece9f977b7c1a46db6f5803316271
HyunSoo Kim <39149858+Nicholas-Kim@users.noreply.github.com>
2021-03-08 21:58:43 +0900

    Update c.java

--- 0304/khs/c.java
+++ 0304/khs/c.java
@@ -1,10 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
 public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = "";
+        input = br.readLine();
+        StringTokenizer st = new StringTokenizer(input," ");
+        int N = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+        ArrayList<Participant> p = new ArrayList<Participant>();
+        for(int i=0;i<N;i++){
+            input = br.readLine();
+            st = new StringTokenizer(input, " ");
+            Participant tmp = new Participant(st.nextToken(),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
+            p.add(tmp);
+        }
+        Collections.sort(p);
+        System.out.println(p.get(R-1).name);
 
 
     }
@@ -26,7 +44,22 @@ class Participant implements Comparable<Participant> {
 
     @Override
     public int compareTo(Participant o) {
-        
-        return 0;
+        //내림차순 정렬 -> (비교대상 -> this)
+        //오름차순 정렬 -> (this -> 비교대상)
+        //가만히 냅두는게 음
+        if(o.score-this.score!=0){
+            return o.score-this.score;
+        }else {
+            if(o.tries-this.tries!=0){
+                return this.tries-o.tries;
+            }else{
+                if(o.time-this.time!=0){
+                    return o.time-this.time;
+                }else{
+                    return this.name.compareTo(o.name);
+                }
+            }
+        }
+
     }
 }
