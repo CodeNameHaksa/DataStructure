bd1904d482bb1b91b317894ffb17ad85f08fb536
HyunSoo Kim <39149858+Nicholas-Kim@users.noreply.github.com>
2021-03-09 12:33:50 +0900

    Add files via upload

---
+++ DS0309mavenprac/example1/src/main/java/org/cscnu/example1/Temperature.java
@@ -0,0 +1,8 @@
+package org.cscnu.example1;
+
+public interface Temperature {
+    public double getCelsius();
+    public double getFahrenheit();
+    public void setCelsius(double celsius);
+    public void setFahrenheit(double fahrenheit);
+}
