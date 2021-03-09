39a0f138c8ea9cedada4a5fbd384ad6a23b5ce76
HyunSoo Kim <39149858+Nicholas-Kim@users.noreply.github.com>
2021-03-09 23:34:16 +0900

    Create eof.java

---
+++ 0309/eof.java
@@ -0,0 +1,19 @@
+//boj 10951
+
+import java.util.*;
+import java.io.*;
+
+public class Main{
+	public static void main(String args[]) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = "";
+        while((s=br.readLine())!=null){
+            String[] ss = s.split(" ");
+         System.out.println(Integer.parseInt(ss[0])+Integer.parseInt(ss[1]));
+              }
+        }       		
+	}
+	
+	
+
