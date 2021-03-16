b5fd471945bf90eeb9d817e23175c5cf87731d02
HyunSoo Kim <39149858+Nicholas-Kim@users.noreply.github.com>
2021-03-17 00:29:04 +0900

    Update boj_16472.java

--- 0315/khs/boj_16472.java
+++ 0315/khs/boj_16472.java
@@ -19,36 +19,38 @@ public class boj_16472 {
 
         while(start<=end){
 
-            if(end==input.length()){
-                if(ans<len&&tmp<N){
+            if(end==input.length()){ //end가 끝까지 간 경우
+                if(ans<len&&tmp<N){ //정답의 업데이트가 필요한 경우. 문자의 종류의 갯수가 조건보다 적으면 답을 업데이트.  
                     ans = len;
                 }
                 break;
             }
 
-            if(tmp>N){
+            if(tmp>N){ //알파벳 종류의 수가 주어진 수보다 클 때
                 indexTmp = input.charAt(start)-97;
-                alpha[indexTmp]--;
-                start++;
-                len--;
-                if(alpha[indexTmp]==0){
+                alpha[indexTmp]--; //알파벳 지우기
+                start++; //start 한 칸 넘겨
+                len--; //길이도 줄인다.
+                if(alpha[indexTmp]==0){ //문자가 사라졌다면, 종류의 갯수를 줄인다.
                     tmp--;
                 }
             }else {
                 indexTmp = input.charAt(end) - 97;
-                if (alpha[indexTmp] == 0) {
-                    tmp++;
+                if (alpha[indexTmp] == 0) { //처음 등장한 알파벳이라면?
+                    tmp++; //문자의 종류 갯수 증가
                 }
-                alpha[indexTmp]++;
-                len++;
-                end++;
-                if (tmp <= N) {
+                alpha[indexTmp]++; //알파벳 등장 기록 증가
+                len++; //문자 길이 증가
+                end++; //현재 알파벳 종류의 수가 주어진 수보다 적거나 같다는 것이므로, end를 한칸 증가시킴.
 
+                if (tmp <= N) { //문제에서 주어진 조건에 부합하면 답을 업데이트.
                     if (len > ans) {
                         ans = len;
                     }
 
                 }
+
+
             }
         }
         System.out.println(ans);
