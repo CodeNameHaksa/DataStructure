18cd84530be075a90990c1ee6ab593d460abb40f
LONGNEW <40235475+LONGNEW@users.noreply.github.com>
2021-03-21 22:24:03 +0900

    Update a.java

--- 0321/longnew/a.java
+++ 0321/longnew/a.java
@@ -10,31 +10,46 @@ public class a {
         StringTokenizer st;
 
         Stack<Character> data = new Stack<>();
+        int flag = 0, cnt = 0;
         String s = "";
-        
         while ((s = br.readLine()) != null) {
-            
-            for (int i = 0; i < s.length(); i++){
+            for (int i = 0; i < s.length(); i++) {
                 char d = s.charAt(i);
+
+                if (flag == 1 || cnt == 1)
+                    continue;
+
+                if (d == '\"'){
+                    if (flag == 0)
+                        flag = 1;
+                    else
+                        flag = 0;
+                    continue;
+                }
+                
+                if (d == '\''){
+                    if (cnt == 0)
+                        cnt = 1;
+                    else
+                        cnt = 0;
+                    continue;
+                }
 
                 if (d == '(' || d == '{' || d == '[')
                     data.push(d);
-                
                 else if (d == ')' || d == '}' || d == ']') {
-                    
+
                     if (data.size() == 0){
                         System.out.println("Compile Error");
                         System.exit(0);
                     }
-                    
+
                     if (d == ')' && data.peek() != '(') {
                         System.out.println("Compile Error");
                         System.exit(0);
-                        
-                    } else if (d == '}' && data.peek() != '{'){
+                    } else if (d == '}' && data.peek() != '{') {
                         System.out.println("Compile Error");
                         System.exit(0);
-                        
                     } else if (d == ']' && data.peek() != '[') {
                         System.out.println("Compile Error");
                         System.exit(0);
@@ -45,7 +60,7 @@ public class a {
         }
         if (data.size() != 0)
             System.out.println("Compile Error");
-        else 
+        else
             System.out.println("No Error");
     }
 }
