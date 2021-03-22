084ff64ed11280cf52b16ed7d19eb02145608e23
LONGNEW <40235475+LONGNEW@users.noreply.github.com>
2021-03-22 16:48:30 +0900

    Create App.java

---
+++ 0321/longnew/data/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+        MyStack stack = new MyStack();
+
+        stack.push("1st");
+        stack.push("2nd");
+        stack.push("3rd");
+
+        System.out.println("stack size: " + stack.size());
+        System.out.println("stack pop: " + stack.pop());
+        System.out.println("stack pop: " + stack.pop());
+        System.out.println("stack size: " + stack.size());
+    }
+}
